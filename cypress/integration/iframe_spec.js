a68981ea8afe6b704aaf4c32a67522ab7596f1cb
Daniel Teoh <danteoh@squareup.com>
2020-07-02 10:30:11 -0700

    Updates to.exist position

--- cypress/integration/iframe_spec.js
+++ cypress/integration/iframe_spec.js
@@ -2,6 +2,6 @@ context('iframes', () => {
   it('Tests that you can touch the data in the iframe', () => {
     cy.visit('http://www.teoh.ca/iframe.html');
     cy.get('iframe')
-      .should(iframe => expect(iframe.contents().find('body').to.exist))
+      .should(iframe => expect(iframe.contents().find('body')).to.exist)
   });
 })
